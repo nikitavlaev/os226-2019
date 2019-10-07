d91913580daa3f1d897e3d9cf1a1e5515bf7884c
Nikita <nikitavlaev00@gmail.com>
2019-10-07 08:52:38 +0300

    Task05_Vlaev

--- cotimer/main.c
+++ cotimer/main.c
@@ -6,20 +6,32 @@
 
 #include "sched.h"
 
-struct app1_aspace {
+static void *alloc(void *pool, int poolsz, void **next, int sz)
+{
+	void *a = *next;
+	*next = (char *)*next + sz;
+	return a;
+}
+#define ALLOC(pool, next) \
+	alloc(pool, sizeof(pool), &next, sizeof(*pool))
+
+struct app1_aspace
+{
 	int id;
 	int duration;
 	int period;
 	int cnt;
 };
 
-static long long reftime(void) {
+static long long reftime(void)
+{
 	struct timespec ts;
 	clock_gettime(CLOCK_BOOTTIME, &ts);
 	return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 }
 
-void app1(void *aspace) {
+void app1(void *aspace)
+{
 	struct app1_aspace *as = aspace;
 	--as->cnt;
 
@@ -27,41 +39,94 @@ void app1(void *aspace) {
 
 	sched_sleep(as->duration);
 
-	printf("%s id %d cnt %d time2 %d reference %lld\n", __func__, as->id, as->cnt, sched_gettime(), reftime());
+	//printf("%s id %d cnt %d time2 %d reference %lld\n", __func__, as->id, as->cnt, sched_gettime(), reftime());
 
-	if (0 < as->cnt) {
+	if (0 < as->cnt)
+	{
 		sched_cont(app1, aspace, as->period);
 	}
 }
 
-int main(int argc, char *argv[]) {
+struct work
+{
+	int duration;
+	int delay_cont;
+};
+struct aireg_space
+{
+	int id;
+	struct work *work;
+};
+void aireg(void *aspace)
+{
+	struct aireg_space *as = aspace;
+	printf("%s id %d dcont %d time1 %d reference %lld\n", __func__, as->id, as->work->delay_cont, sched_gettime(), reftime());
+
+	sched_sleep(as->work->duration);
+
+	//printf("%s id %d dcont %d time2 %d reference %lld\n", __func__, as->id, as->work->delay_cont, sched_gettime(), reftime());
+
+	if (0 <= as->work->delay_cont)
+	{
+		sched_cont(app1, aspace, as->work->delay_cont);
+		++as->work;
+	}
+}
+
+int main(int argc, char *argv[])
+{
 	char name[64];
 	int prio;
-	int duration;
-	int period;
-	int cnt;
 
 	printf("reftime %lld\n", reftime());
 
-	struct app1_aspace a1as[16];
-	int a1as_n = 0;
-	while (EOF != scanf("%s %d %d %d %d", name, &prio, &duration, &period, &cnt)) {
-		struct app1_aspace *as = &a1as[a1as_n++];
+	struct app1_aspace a1pool[16];
+	void *a1next = a1pool;
 
-		as->id = as - a1as;
-		as->duration = duration;
-		as->period = period;
-		as->cnt = cnt;
+	struct work workpool[128];
+	void *worknext = workpool;
+	struct aireg_space airpool[16];
+	void *airnext = airpool;
 
-		void (*entry)(void*);
-		if (!strcmp("app1", name)) {
-			entry = app1;
-		} else {
+	while (EOF != scanf("%s %d", name, &prio))
+	{
+		void *asp;
+		if (!strcmp("app1", name))
+		{
+			int duration;
+			int period;
+			int cnt;
+			scanf("%d %d %d", &duration, &period, &cnt);
+
+			struct app1_aspace *as = ALLOC(a1pool, a1next);
+			as->id = as - a1pool;
+
+			as->duration = duration;
+			as->period = period;
+			as->cnt = cnt;
+
+			sched_new(app1, as, prio);
+		}
+		else if (!strcmp("aireg", name))
+		{
+			struct aireg_space *as = ALLOC(airpool, airnext);
+			as->id = as - airpool;
+			as->work = worknext;
+
+			struct work *w;
+			do
+			{
+				w = ALLOC(workpool, worknext);
+				scanf("%d %d", &w->duration, &w->delay_cont);
+			} while (w->delay_cont != -1);
+
+			sched_new(aireg, as, prio);
+		}
+		else
+		{
 			fprintf(stderr, "Unknown app: %s\n", name);
 			return 1;
 		}
-
-		sched_new(entry, as, prio);
 	}
 
 	sched_run(100);
--- cotimer/sched.c
+++ cotimer/sched.c
@@ -1,35 +1,197 @@
 #include <stdbool.h>
+#include <unistd.h>
 #include <stdio.h>
 
 #include "timer.h"
 #include "sched.h"
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*a))
+#include <stdlib.h>
 
-static void sigalrm(int sig) {
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(*a))
+#define SCHEDULE_MAX_SIZE 1000
+#define INF 10000
+
+int periods;
+int timer_period;
+
+struct Task
+{
+	void (*entrypoint)(void *aspace);
+	void *aspace;
+	int priority;
+	int actual_priority;
+	int timeout;
+};
+
+typedef struct Task Task;
+
+Task schedule[SCHEDULE_MAX_SIZE];
+int schedule_size = 0;
+char update = 0;
+
+Task create_task(void (*entrypoint)(void *aspace),
+				 void *aspace,
+				 int priority,
+				 int timeout)
+{
+	Task buf;
+	buf.entrypoint = entrypoint;
+	buf.aspace = aspace;
+	buf.priority = priority;
+	buf.actual_priority = priority;
+	buf.timeout = timeout;
+	return buf;
+}
+
+void sched_sort()
+{
+	char more(int i, int j)
+	{
+		if (schedule[i].timeout <= schedule[j].timeout)
+		{
+			if (schedule[i].timeout < schedule[j].timeout)
+			{
+				return 1;
+			}
+			else
+			{
+				if (schedule[i].priority = schedule[j].priority)
+				{
+					return 1;
+				}
+				else
+				{
+					return 0;
+				}
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	void swap(int i, int j)
+	{
+		Task buf = schedule[i];
+		schedule[i] = schedule[j];
+		schedule[j] = buf;
+	}
+	
+	void quickSort(int first, int last)
+	{
+		int i, j, pivot, temp;
+
+		if (first < last)
+		{
+			pivot = first;
+			i = first;
+			j = last;
+
+			while (i < j)
+			{
+				while ((more(i,pivot)) && i < last)
+					i++;
+				while (!more(j, pivot))
+					j--;
+				if (i < j)
+				{
+					swap(i,j);
+				}
+			}
+
+			swap(j, pivot);
+			quickSort(first, j - 1);
+			quickSort(j + 1, last);
+		}
+	}
+	quickSort(0, schedule_size - 1);
+}
+void sched_update()
+{
+	int current_time = sched_gettime();
+
+	for (int i = 0; i < schedule_size; i++)
+	{
+		if (schedule[i].timeout <= current_time)
+		{
+			schedule[i].timeout = 0;
+		}
+		printf(" %d %d ", schedule[i].actual_priority, schedule[i].timeout);
+	}
+	printf("\n");
+
+	schedule[0].timeout = INF;
+	sched_sort();
+	schedule_size--; //delete old task
+
+	printf("after sort \n");
+	for (int i = 0; i < schedule_size; i++)
+	{
+		printf(" %d %d ", schedule[i].actual_priority, schedule[i].timeout);
+	}
+	printf("\n");
+	printf("\n");
+}
+
+static void sigalrm(int sig)
+{
 	fprintf(stderr, "%s\n", __func__);
+	periods++;
 }
 
 void sched_new(void (*entrypoint)(void *aspace),
-		void *aspace,
-	       	int priority) {
+			   void *aspace,
+			   int priority)
+{
+	schedule[schedule_size] = create_task(entrypoint, aspace, priority, 0);
+	schedule_size++;
 }
 
 void sched_cont(void (*entrypoint)(void *aspace),
-		void *aspace,
-		int timeout) {
+				void *aspace,
+				int timeout)
+{
+	//priority from current task
+	printf("cont timeout %d time %d\n", timeout, sched_gettime());
+	schedule[schedule_size] = create_task(entrypoint, aspace, schedule[0].actual_priority + 1, sched_gettime() + timeout);
+	schedule_size++;
 }
 
-void sched_sleep(unsigned ms) {
+void sched_sleep(unsigned ms)
+{
+
+	int out_ms = sched_gettime() + ms;
+	printf("sleep %d ", out_ms);
+	while (sched_gettime() < out_ms)
+		;
+	printf("sleep-end %d %d\n", sched_gettime(), out_ms);
+	update = 1;
 }
 
-int sched_gettime(void) {
-	return 0;
+int sched_gettime(void)
+{
+	int time = periods * timer_period + timer_cnt();
+	return time;
 }
 
-void sched_run(int period_ms) {
+void sched_run(int period_ms)
+{
 	timer_init_period(period_ms, sigalrm);
-	for (int i = 0; i < 10; ++i) {
-		fprintf(stderr, "timer cnt %d\n", timer_cnt());
+	periods = 0;
+	timer_period = period_ms;
+	while (schedule_size > 0)
+	{
+		if (schedule[0].timeout <= sched_gettime())
+		{
+			(schedule[0].entrypoint)(schedule[0].aspace);
+		}
+		if (update)
+		{
+			printf("update \n");
+			sched_update();
+			update = 0;
+			//choose new task -> 0 indexed
+		}
 	}
 }
